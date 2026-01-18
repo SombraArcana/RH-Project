f8a971d1973ce753b37dd4ac388b2b3864759ede
Pedro <pamfigueiredo18@gmail.com>
2026-01-18 10:39:43 -0300

    Corrigindo submódulo e adicionando src como pasta normal

---
+++ src/GUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class GUI {
+    private JPanel panel1;
+    private JButton button1;
+    private JLabel labelContador;
+
+    public GUI() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String texto = labelContador.getText();
+                int valorAtual = Integer.parseInt(texto);
+                int novoValor = valorAtual + 5;
+                labelContador.setText(String.valueOf(novoValor));
+            }
+        });
+    }
+
+    public JPanel getRootPanel() {
+        return panel1; 
+    }
+}
